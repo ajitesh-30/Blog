24634f3c6596133f206213b945f1d2e02e3f4fa4
Ajitesh <ajiteshs10@gmail.com>
2018-01-21 19:08:12 +0530

    After storing static files

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
+class HomesTests(TestCase):
+	def test_home_view_status_code(self):
+		url = reverse('home')
+		response = self.client.get(url)
+		self.assertEquals(response.status_code,200)
+
+	def test_home_url_resolvers_home_views(self):
+		view = resolve('/')
+		self.assertEquals(view.func,home)
 # Create your tests here.
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
+
+def home(request):
+	boards = Board.objects.all()
+	return render(request,'index.html',{'boards':boards})
 
 # Create your views here.
