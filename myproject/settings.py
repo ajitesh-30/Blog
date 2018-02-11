df645fc385ca44a416f77687ca9625931429a6e1
Ajitesh <ajiteshs10@gmail.com>
2018-02-11 16:12:11 +0530

    Before deloyment

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'widget_tweaks',
     'boards',
     'accounts',
@@ -128,5 +129,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 
+
+LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL= 'index'
-LOGIN_REDIRECT_URL = 'login'
